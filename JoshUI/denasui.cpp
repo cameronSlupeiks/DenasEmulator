6f31448817b4e20e75a6348c02dc60a0cbb63659
student <supreraul_58@yahoo.com>
2020-03-26 15:15:51 -0400

    battery stats

--- JoshUI/denasui.cpp
+++ JoshUI/denasui.cpp
@@ -1,5 +1,11 @@
 #include "denasui.h"
 #include "ui_denasui.h"
+#include <iostream>
+
+
+QProgressBar *progressBar;
+
+int battery =100;
 
 DenasUI::DenasUI(QWidget *parent) : QMainWindow(parent), ui(new Ui::DenasUI)
 {
@@ -11,3 +17,18 @@ DenasUI::~DenasUI()
     delete ui;
 }
 
+
+void DenasUI::on_pushButton_6_clicked(bool checked)
+{
+    battery = battery - 10;
+    std::cout<<battery<< std::endl;
+   // progressBar->setValue(bat);
+}
+
+
+
+
+void DenasUI::on_progressBar_valueChanged(int value)
+{
+
+}
